302d561b4f5998fc7869c927902a890807a5172f
moxirboy <Moxirboy.M@iman.ux>
2023-11-22 15:39:45 +0500

    .

--- cmd/app/root.go
+++ cmd/app/root.go
@@ -1,24 +1,12 @@
 package app
 
 import (
-	configs "bug-free/internal/config"
 	http2 "bug-free/internal/http"
 	"bug-free/internal/server"
-	"github.com/spf13/cobra"
 	"log"
 )
 
-var rootCmd = &cobra.Command{
-	Use: "http-server",
-	Run: func(cmd *cobra.Command, args []string) {
-		srv := new(server.Server)
-		port := configs.Configuration().RunPort
 
-		if err := srv.Run(port, http2.Router()); err != nil {
-			log.Fatalf("error occured")
-		}
-	},
-}
 
 
 // @title Your API Title
@@ -28,10 +16,7 @@ var rootCmd = &cobra.Command{
 // @BasePath /api/v1
 
 func Execute() {
-	//if err := rootCmd.Execute(); err != nil {
-	//	fmt.Fprintf(os.Stderr, "error while executing your CLI '%s'", err)
-	//	os.Exit(1)
-	//}
+
 	srv := new(server.Server)
 	port := "3000"
 	log.Print("started")
